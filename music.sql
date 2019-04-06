1b8126a4fcbaac347510a4abe1df9d834fd687c0
d-vega <15893653+d-vega@users.noreply.github.com>
2019-04-06 02:16:52 -0400

    Database schema

---
+++ music.sql
@@ -0,0 +1,7 @@
+CREATE TABLE artists (music_artist TEXT,
+                      album TEXT);
+
+CREATE TABLE albums (song_name TEXT,
+                     track_num INTEGER,
+                     song_length INTEGER,
+                     album TEXT);
