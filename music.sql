4e127dee7c227a2bcad779a436d313bd3db05b3f
d-vega <15893653+d-vega@users.noreply.github.com>
2019-04-06 02:22:26 -0400

    Adjusting database schema

--- music.sql
+++ music.sql
@@ -4,4 +4,4 @@ CREATE TABLE artists (music_artist TEXT,
 CREATE TABLE albums (song_name TEXT,
                      track_num INTEGER,
                      song_length INTEGER,
-                     album TEXT);
+                     FOREIGN KEY (album) REFERENCES artists(album));
