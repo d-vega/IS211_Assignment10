4d6aed38fcc3a44c764356af7032de3acb215b7c
d-vega <15893653+d-vega@users.noreply.github.com>
2019-04-07 18:26:27 -0400

    Completed schema creation

--- music.sql
+++ music.sql
@@ -1,7 +1,21 @@
-CREATE TABLE artists (music_artist TEXT,
-                      album TEXT);
+/*
+If schema is used in sqlite3,
+please run the following beforehand:
 
-CREATE TABLE albums (song_name TEXT,
-                     track_num INTEGER,
-                     song_length INTEGER,
-                     FOREIGN KEY (album) REFERENCES artists(album));
+  PRAGMA foreign_keys = ON;
+
+*/
+
+CREATE TABLE artists (
+  album_id INTEGER NOT NULL PRIMARY KEY,
+  music_artist TEXT
+  );
+
+CREATE TABLE albums (
+  song_name TEXT,
+  track_num INTEGER,
+  song_length INTEGER,
+  album TEXT,
+  album_id INTEGER,
+  FOREIGN KEY(album_id) REFERENCES artists(album_id)
+  );
